16cac784ce8176644bdb60f7266fada7f5891c53
Alma Cleto <emscleto11@gmail.com>
2023-02-02 05:28:22 +0800

    create changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App'
 // import './css/index.css';
 // import './css/styles.css';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <Router>
-      <App />
-    </Router>
-    </React.StrictMode>
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App/>
+      </HashRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
   );
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root"));
+//   root.render(
+//     <React.StrictMode>
+//       <Router>
+//       <App />
+//     </Router>
+//     </React.StrictMode>
+//   );
 
   reportWebVitals();
